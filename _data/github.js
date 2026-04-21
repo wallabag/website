c25b667ad249fd80e87c81a04335f5637faba6fb
Jeremy Benoist <jeremy.benoist@gmail.com>
2026-04-21 20:59:39 +0200

    Remove `node-fetch` to use native fetch
    Set minimum node version to allow it.
    Also bump all deps.

--- _data/github.js
+++ _data/github.js
@@ -1,11 +1,9 @@
-const fetch = require("node-fetch");
-
 module.exports = async function() {
   console.log( "Fetching Github latest release" );
 
   // GitHub API: https://developer.github.com/v3/repos/#get
   return fetch("https://api.github.com/repos/wallabag/wallabag/releases/latest")
-    .then(res => res.json()) // node-fetch option to transform to json
+    .then(res => res.json())
     .then(json => {
       // prune the data to return only what we want
       return {
